5e0fcfed45ec62a8edbfbd6d8c33cb228490aeb4
franpaulino <franpaulinoinfan@gmail.com>
2025-03-20 16:24:47 -0400

    Update

---
+++ QuestionnaireMathGameApp/QuestionnaireMathGameConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Title = "Questionnaire Math Game App";
+
+
+Console.ReadKey();
+
+/*
+        Requisitos
+            - Debe tener las operaciones básicas (suma, resta, multiplicación y división).
+            - Solo funciona con número enteros positivos menores o igual a 100.
+            - En la división solo deben ser divisiones que resulten en números enteros.
+            - Se debe presentar un menú con las opciones.
+            - Se debe almacenar los resultados de cada juego.
+            - Se debe implementar un nivel de dificulta basico (facil, medio, dificil, aleatorio)
+                - Debe implemetnarse con un tiempo
+                    - Facíl: 45 segundos para dar una respuesta y las operaciones no deben superar los priemros 333 primeros números enteros (ejemplo: 33 + 33).
+                    - Medio: 30 segundos para dar una respuesta y las operaciones no deben superar los priemros 666 primeros números enteros (ejemplo: 66 - 66).
+                    - Dificil: 15 segundo para dar una respuesta y las operaciones no deben superar el tope permitido en la aplicación "1000" (ejemplo: 100 / 100).
+                    - Aleatorio: Las pregutnas son aleatorio, sin tomar el cuenta el nivel de dificiltad
+
+        Objetivo
+            Se trata de construir una aplicacicón de un juego de matematicas que contenga las 4 operaciones basicas, suma, resta, multiplicacion y division.
+            Solo debe presentar números enteros, por lo tanto, en la division el dividendo no puede ser un número impar, ni menor que el dividor.
+            Se debe guardar cada ejercicio en una lista.
+                
+    */
